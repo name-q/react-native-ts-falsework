979d326262fb987f286ecf0d402ca894370da5a4
钱雨 <1620206666@qq.com>
2021-12-21 16:24:55 +0800

    删除绝对路径演示数据

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,6 @@ import {
     Text,
 } from 'react-native';
 
-import AssetsDemo from '@/assets/demo'
-import ComponentsDemo from '@/components/demo'
-import ConfigDemo from '@/config/demo'
-import ModelsDemo from '@/models/demo'
-import NavigatorDemo from '@/navigator/demo'
-import PagesDemo from '@/pages/demo'
-import UtilssDemo from '@/utils/demo'
 import Config from "react-native-config";
 
 const App: React.FC = () => (
@@ -25,13 +18,6 @@ const App: React.FC = () => (
                     react-native-config  BASE_URL = {Config.BASE_URL}
                 </Text>
             </View>
-            <AssetsDemo />
-            <ComponentsDemo />
-            <ConfigDemo />
-            <ModelsDemo />
-            <NavigatorDemo />
-            <PagesDemo />
-            <UtilssDemo />
         </ScrollView>
     </SafeAreaView>
 );
