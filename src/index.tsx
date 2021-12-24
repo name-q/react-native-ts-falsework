077c95e9b691512bae423a842c50526e52eb39b5
钱雨 <1620206666@qq.com>
2021-12-24 18:26:18 +0800

    安卓顶部 5G wifi等信息展示条   barStyle="dark-content"去掉可以隐藏

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import AppStack from '@/navigator/stack'
+import { StatusBar } from 'react-native';
 
 const App: React.FC = () => (
     <SafeAreaProvider>
         <NavigationContainer>
             <AppStack />
         </NavigationContainer>
+        <StatusBar backgroundColor="transparent" barStyle="dark-content" translucent />
     </SafeAreaProvider>
 );
 
